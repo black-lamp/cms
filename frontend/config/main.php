e58642bc616752d803f56a1feaf11ae7d023740c
Вадим <guts.vadim@gmail.com>
2016-05-04 19:44:07 +0300

    seo url rules and theme config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -18,6 +18,19 @@ return [
     ],
 
     'components' => [
+        'request' => [
+            'baseUrl' => '/',
+        ],
+        'view' => [
+            'theme' => [
+                'basePath' => '@frontend/themes/' . $params['themeName'],
+                'baseUrl' => '@web/themes/' . $params['themeName'],
+                'pathMap' => [
+                    '@frontend/views' => '@frontend/themes/' . $params['themeName'] . '/views',
+                    '@vendor/black-lamp/yii2-articles/frontend/views' => '@frontend/themes/' . $params['themeName'] . '/modules/yii2-articles'
+                ],
+            ],
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
@@ -32,15 +45,37 @@ return [
             ],
         ],
         'errorHandler' => [
-            'errorAction' => 'site/error',
+            'errorAction' => '/error',
         ],
         'urlManager' => [
+            'class' => 'bl\multilang\MultiLangUrlManager',
             'baseUrl' => '/',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'cache' => null,
             'rules' => [
+                /*[
+                    'class' => 'bl\seo\SeoUrlRule',
+                    'modelClass' => \bl\articles\entities\ArticleTranslation::className(),
+//                    'prefix' => 'articles/',
+                    'route' => 'articles/index',
+                    'params' => [
+                        'id' => 'article_id'
+                    ],
+                    'condition' => [
+                        'language_id' => function() {
+                            return \bl\multilang\entities\Language::getCurrent()->id;
+                        }
+                    ]
+                ]*/
+                [
+                    'class' => 'bl\articles\UrlRule',
+                ]
             ],
         ],
+    ],
+    'as RedirectBehavior' => [
+        'class' => 'bl\cms\redirect\behaviors\RedirectBehavior'
     ],
     'params' => $params,
 ];
